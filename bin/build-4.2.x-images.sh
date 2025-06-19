a05d2012b52db7978469eb12061227ff871a2e01
Demin Yin <deminy@deminy.net>
2025-06-19 10:26:43 -0700

    add and update Docker scripts

--- bin/build-4.2.x-images.sh
+++ bin/build-4.2.x-images.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env ksh
 #
-# This script builds Docker images for the PHP Couchbase extension version 4.3.x.
+# This script builds Docker images for the PHP Couchbase extension version 4.2.x.
 #
 # Usage:
-#   ./build-4.3.x-images.sh <couchbase_version>
+#   ./build-4.2.x-images.sh <couchbase_version>
 # Example:
-#   ./build-4.3.x-images.sh 4.3.0 2>&1 | tee ./build-4.3.0-images.log
+#   ./build-4.2.x-images.sh 4.2.7 2>&1 | tee ./build-4.2.7-images.log
 #
 
 set -e
 set +x
 
-# Check if at least one argument is provided, and the first argument starts with "4.3.".
-if [[ $# -lt 1 || ! $1 =~ ^4\.3\. ]] ; then
+# Check if at least one argument is provided, and the first argument starts with "4.2.".
+if [[ $# -lt 1 || ! $1 =~ ^4\.2\. ]] ; then
     echo "Usage: $0 <couchbase_version>"
-    echo "Example: $0 4.3.0"
+    echo "Example: $0 4.2.0"
     exit 1
 fi
 
@@ -24,10 +24,10 @@ cd "${CURRENT_DIR}/.."
 
 couchbase_version=$1
 matrix=(
+  ("8.0" "no-debug-non-zts-20200930")
   ("8.1" "no-debug-non-zts-20210902")
   ("8.2" "no-debug-non-zts-20220829")
   ("8.3" "no-debug-non-zts-20230831")
-  ("8.4" "no-debug-non-zts-20240924")
 )
 
 docker buildx rm -f --builder docker-php-couchbase
@@ -40,12 +40,12 @@ for i in ${!matrix[@]}; do
 
     (set -x; docker buildx build --progress=plain \
       --platform linux/amd64,linux/arm64/v8 \
-      --build-arg SWOOLE_IMAGE_TAG=6.0-php${php_version} \
+      --build-arg SWOOLE_IMAGE_TAG=5.1-php${php_version} \
       --build-arg COUCHBASE_VERSION=${couchbase_version} \
       --build-arg PHP_EXTENSION_DIR=${ext_dir} \
       --tag deminy/php-couchbase:${couchbase_version}-php${php_version} \
       --output=type=registry,registry.insecure=true \
-      ./dockerfiles/4.3.x/)
+      ./dockerfiles/4.2.x/)
 
     # Check the image manifest list from the registry:.
     (set -x; docker buildx imagetools inspect deminy/php-couchbase:${couchbase_version}-php${php_version})
